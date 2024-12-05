aa415c91fa79af2a5eb0f8153f7d5e4a3aa3ad2f
Frong12345 <frong_5057@hotmail.co.th>
2024-12-05 20:42:06 +0700

    Left Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import './App.css'
 function App() {
   
   return (
-    <div className='mx-auto max-w-6xl grid grid-cols-[40%_60%]'>
+    <div className='mt-14 mx-auto max-w-6xl grid grid-cols-[40%_60%]'>
       <div>
-        <div className='sticky top-0 grid grid-rows-[35%_45%_20%] h-screen'>
-          <div>Title</div>
+        <div className='sticky top-14 grid grid-rows-[35%_45%_20%] h-screen'>
+          <div>
+            <div>Wiwat Chaichana</div>
+            <div>Software Developer-between jobs</div>
+            <div>From concept reality, bridging the gap with code, tranforming ideas into tangible digital solution.</div>
+            <div>Viwe Resume</div>
+          </div>
           <div>Nav</div>
           <div className='flex items-end'>Link</div>
         </div>
